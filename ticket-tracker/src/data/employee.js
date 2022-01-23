edde949d886501d8b72033926db5bbde150be557
4naans <mylespillay@yahoo.co.uk>
2022-01-23 13:56:34 +0000

    set up component tree and style files

---
+++ ticket-tracker/src/data/employee.js
@@ -0,0 +1,64 @@
+const team = [ 
+    { 
+      id: 1, 
+      name: "Alima Miller", 
+      role: "Junior Software Developer" 
+    }, 
+    { 
+      id: 2, 
+      name: "Evelyn Rodgers", 
+      role: "Junior Software Developer" 
+    }, 
+    { 
+      id: 3, 
+      name: "Henley Myers", 
+      role: "Junior Software Developer" 
+    }, 
+    { 
+      id: 4,
+      name: "Celyn Sanders", 
+      role: "Software Developer" 
+    }, 
+    { 
+      id: 5, 
+      name: "Effie Mohamed", 
+      role: "Software Developer" 
+    }, 
+    { 
+      id: 6, 
+      name: "Karam Lees", 
+      role: "Senior Software Developer" 
+    }, 
+    { 
+      id: 7, 
+      name: "Eleanor Dowling", 
+      role: "Project Manager" 
+    }, 
+    { 
+      id: 8, 
+      name: "Haris Grey",
+
+    role: "Tester" 
+}, 
+{ 
+  id: 9, 
+  name: "Sohaib Farley", 
+  role: "Tester" 
+}, 
+{ 
+  id: 10, 
+  name: "Phillip Mitchell", 
+  role: "Hard Man" 
+}, 
+{ 
+  id: 11, 
+  name: "Dave Grohl", 
+  role: "Hard Man" 
+}, 
+{ 
+  id: 12, 
+  name: "John Frusciante", 
+  role: "Hard Man" 
+}, 
+] 
+export default team; 
